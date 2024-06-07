074e8f5b3d3f567a3ae1b3e6eb6c38285c7b1212
Charles <Charleskojomark>
2024-06-07 11:08:01 +0100

    render deploy

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+if [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser \
+        --no-input \
+        --email "$DJANGO_SUPERUSER_EMAIL" \
+        --username "$DJANGO_SUPERUSER_USERNAME"
+fi
 # Convert static asset files
 python manage.py collectstatic --no-input
 
