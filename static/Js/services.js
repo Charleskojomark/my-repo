108a3c10cd79d20cb5543f5ab1152076e6a5f75e
Charles <Charleskojomark>
2024-06-24 15:45:17 +0100

    updated

--- static/Js/services.js
+++ static/Js/services.js
@@ -1,7 +1,6 @@
-// services.js
 document.addEventListener("DOMContentLoaded", function() {
-    // Check if there is a category stored in local storage
-    const activeCategory = localStorage.getItem('activeCategory');
+    const urlParams = new URLSearchParams(window.location.search);
+    const activeCategory = urlParams.get('category') || localStorage.getItem('activeCategory');
     if (activeCategory) {
         const activeCard = document.querySelector(`.card[data-category="${activeCategory}"]`);
         if (activeCard) {
@@ -10,9 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
             const subCategoryDisplay = document.querySelector('.sub-category-content');
             subCategoryDisplay.innerHTML = subCategoryContent;
             subCategoryDisplay.style.display = 'flex';
+            localStorage.setItem('activeCategory', activeCategory);  // Store active category in localStorage
         }
     } else {
-        // If no category is stored, make the first card active
         const firstCard = document.querySelector('.card');
         if (firstCard) {
             firstCard.classList.add('active');
@@ -27,14 +26,13 @@ document.addEventListener("DOMContentLoaded", function() {
 function showSubCategory(card) {
     const cards = document.querySelectorAll('.card');
     cards.forEach(c => c.classList.remove('active'));
-    
+
     card.classList.add('active');
     const subCategoryContent = card.querySelector('.sub-category').innerHTML;
     const subCategoryDisplay = document.querySelector('.sub-category-content');
     subCategoryDisplay.innerHTML = subCategoryContent;
     subCategoryDisplay.style.display = 'flex';
 
-    // Store the active category in local storage
     const activeCategory = card.getAttribute('data-category');
     localStorage.setItem('activeCategory', activeCategory);
 }
